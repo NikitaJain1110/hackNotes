4f377529ca49b1c123738b2e4960d8adaf097be7
NikitaJain1110 <56667079+NikitaJain1110@users.noreply.github.com>
2020-05-03 18:17:05 +0530

    app.js update

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+var express    = require('express');
+var bodyParser = require('body-parser');
+var app        = express();
 
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+
+app.listen(3000, function()
+{
+    console.log("SERVER IS RUNNING")
+})
